98b176143d46a96265e8846c56c8ebecb3e8bdb4
Somya Pal <153810729+somyaa1707@users.noreply.github.com>
2025-06-07 21:45:24 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,40 @@
+let box = document.getElementById("box");
+let result = document.getElementById("result");
+let message = document.getElementById("message");
+let startTime, endTime;
+
+function getRandomTime() {
+  return Math.random() * 3000 + 2000; // Between 2 to 5 seconds
+}
+
+function showBox() {
+  let top = Math.random() * 300;
+  let left = Math.random() * 300;
+  let color = "#" + Math.floor(Math.random()*16777215).toString(16);
+
+  box.style.top = top + "px";
+  box.style.left = left + "px";
+  box.style.backgroundColor = color;
+  box.style.display = "block";
+  startTime = Date.now();
+}
+
+function startGame() {
+  box.style.display = "none";
+  result.textContent = "";
+  message.textContent = "Wait for it...";
+
+  setTimeout(() => {
+    showBox();
+    message.textContent = "Click it!";
+  }, getRandomTime());
+}
+
+box.onclick = function () {
+  endTime = Date.now();
+  let reactionTime = (endTime - startTime) / 1000;
+  alert(reactionTime)
+  startGame(); // Restart automatically
+};
+
+startGame();
